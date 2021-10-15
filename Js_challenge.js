df9b225d65edff916957101ad4a2a8a83721c93f
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2021-10-15 22:42:42 +0330

    First Challenge Done

---
+++ Js_challenge.js
@@ -0,0 +1,8 @@
+/* Challenge Number 1*/
+/* Difficulty: very easy*/
+/*Create a function that takes two numbers as arguments and return their sum.*/
+
+function func(a, b) {
+    var result = a + b;
+}
+let x = func(4, 3);
