7d5bc6488af03da2af7a1672822f1314f088da23
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2021-10-15 23:19:40 +0330

    second challenge Done

--- Js_challenge.js
+++ Js_challenge.js
@@ -1,8 +1,15 @@
 /* Challenge Number 1*/
 /* Difficulty: very easy*/
 /*Create a function that takes two numbers as arguments and return their sum.*/
+let x = func(4, 3);
 
 function func(a, b) {
-    var result = a + b;
+    return a + b;
 }
-let x = func(4, 3);
+/* Challenge Number 2*/
+/* Difficulty: very easy*/
+/*Write a function that takes an integer minutes and converts it to seconds..*/
+var minut = 3
+function func2(minut) {
+    return result * 60;
+}
